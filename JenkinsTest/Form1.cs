b4171bb7075971d7fa25bd34a08b2e24984a4d25
Kay <2063375blaauw@zuyd.nl>
2023-05-11 12:57:59 +0200

    IUHAIDUHAIUH

--- JenkinsTest/Form1.cs
+++ JenkinsTest/Form1.cs
@@ -6,5 +6,10 @@ namespace JenkinsTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
